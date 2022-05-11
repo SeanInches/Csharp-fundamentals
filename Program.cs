94ef844d06e1cd96ee278a5cd48f8464d7aaa92d
Sean <seanminches@gmail.com>
2022-05-10 20:31:23 -0400

    the basics

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyUtilities;
+
+namespace CsharpFundamentals
+{
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            WeatherUtilities.Report("Georgia", 23, 65); // 73F
+        }
+
+    }
+    
+}
